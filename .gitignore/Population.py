2e749bd822b5d5b7ed6d689f3412d2d26541945c
Berkay <32465888+DevHunterYZ@users.noreply.github.com>
2017-10-21 18:03:07 +0300

    Update Population.py

--- .gitignore/Population.py
+++ .gitignore/Population.py
@@ -1,13 +1,1 @@
-import pandas as pd
-import seaborn as sns
-import statsmodels.formula.api as smf
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-from sklearn.cross_validation import train_test_split
-import numpy as np
 
-# allow plots to appear directly in the notebook
-%matplotlib inline
-headers = ['Year','Population']
-data = pd.read_csv('C:/Users/user/Desktop/population.csv', delim_whitespace=True, names=headers )
-print (data)
